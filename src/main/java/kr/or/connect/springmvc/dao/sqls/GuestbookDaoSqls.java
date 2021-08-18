91421c3388fcaa748b7dd59b5daaaa3546852ab7
wdEffort <yoman3011@gmail.com>
2021-08-19 01:26:34 +0900

    레이어드 아키텍처 - Guestbook 프로젝트 Controller 구현

--- src/main/java/kr/or/connect/springmvc/dao/sqls/GuestbookDaoSqls.java
+++ src/main/java/kr/or/connect/springmvc/dao/sqls/GuestbookDaoSqls.java
@@ -6,6 +6,6 @@ package kr.or.connect.springmvc.dao.sqls;
 public class GuestbookDaoSqls {
 
     public static final String SELECT_COUNT = "SELECT COUNT(*) FROM boost_guestbook WHERE 1 = 1";
-    public static final String SELECT_PAGING = "SELECT id, name, content, regdate FROM boost_guestbook WHERE 1 = 1 ORDRE BY id DESC LIMIT :start, :limit";
+    public static final String SELECT_PAGING = "SELECT id, name, content, regdate FROM boost_guestbook WHERE 1 = 1 ORDER BY id DESC LIMIT :start, :limit";
     public static final String DELETE_BY_ID = "DELTE FROM boost_guestbook WHERE 1 = 1 AND id = :id";
 }
